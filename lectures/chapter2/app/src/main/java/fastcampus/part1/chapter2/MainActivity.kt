2f48734eccbac776d8cd598c84d0d9397f6d1150
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-09-07 17:53:41 +0900

    Part1.Ch02.숫자세기 앱
    숫자세기앱 개발

--- lectures/chapter2/app/src/main/java/fastcampus/part1/chapter2/MainActivity.kt
+++ lectures/chapter2/app/src/main/java/fastcampus/part1/chapter2/MainActivity.kt
@@ -2,10 +2,58 @@ package fastcampus.part1.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("ActivityLog", "Create")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextViewId)
+        val resetButton = findViewById<Button>(R.id.resetButtonId)
+        val plusButton = findViewById<Button>(R.id.plusButtonId)
+
+        numberTextView.text = number.toString()
+
+        resetButton.setOnClickListener {
+            Log.d("onClick", "리셋 버튼이 클릭 됐습니다.")
+            number = 0
+            numberTextView.text = number.toString()
+        }
+
+        plusButton.setOnClickListener {
+            Log.d("onClick", "더하기 버튼이 클릭 됐습니다.")
+            number++
+            numberTextView.text = number.toString()
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityLog", "Start")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityLog", "Resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityLog", "Resume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityLog", "Stop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityLog", "Destroy")}
 }
