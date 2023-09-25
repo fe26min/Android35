78a13545104ffba98ab04723bfa7505ad2c3c695
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-09-26 08:47:58 +0900

    part01.ch09.02.MediaPlayer로 음원재생(1)
    MediaPlayer를 이용하여 영상 재생

---
+++ lectures/Chapter9/app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
@@ -0,0 +1,40 @@
+package fastcampus.part1.chapter9
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fastcampus.part1.chapter9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer : MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener{ mediaPlayerPause() }
+        binding.stopButton.setOnClickListener{ mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerPlay() {
+        if(mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.cheer).apply {
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+}
