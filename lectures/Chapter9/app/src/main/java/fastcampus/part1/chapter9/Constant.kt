3a3b906d052a35d46d5cb2b491205218b1b8fcfa
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-09-26 09:01:43 +0900

    part01.ch09.03.MediaPlayer로 음원재생 (2)
    서비스를 이용하여 음원을 재생하는 방법

---
+++ lectures/Chapter9/app/src/main/java/fastcampus/part1/chapter9/Constant.kt
@@ -0,0 +1,5 @@
+package fastcampus.part1.chapter9
+
+const val MEDIA_PLAYER_PLAY = "play"
+const val MEDIA_PLAYER_PAUSE = "pause"
+const val MEDIA_PLAYER_STOP = "stop"
--- lectures/Chapter9/app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
+++ lectures/Chapter9/app/src/main/java/fastcampus/part1/chapter9/MainActivity.kt
@@ -1,5 +1,6 @@
 package fastcampus.part1.chapter9
 
+import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -20,21 +21,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun mediaPlayerPlay() {
-        if(mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.cheer).apply {
-                isLooping = true
-            }
-        }
-        mediaPlayer?.start()
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PLAY }
+        startService(intent)
+
     }
 
     private fun mediaPlayerPause() {
-        mediaPlayer?.pause()
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PAUSE }
+        startService(intent)
     }
 
     private fun mediaPlayerStop() {
-        mediaPlayer?.stop()
-        mediaPlayer?.release()
-        mediaPlayer = null
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_STOP }
+        startService(intent)
     }
 }
