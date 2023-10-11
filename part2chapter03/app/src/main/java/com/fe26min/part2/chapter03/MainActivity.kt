e3c389568ee4a4ec69cc07adbb15de9bc6dc9a87
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-10-11 23:07:02 +0900

    35.part02.ch03.imsi
    35.part02.ch03.imsi

--- part2chapter03/app/src/main/java/com/fe26min/part2/chapter03/MainActivity.kt
+++ part2chapter03/app/src/main/java/com/fe26min/part2/chapter03/MainActivity.kt
@@ -7,7 +7,10 @@ import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.net.HttpURLConnection
 import java.net.ServerSocket
+import java.net.Socket
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,37 +18,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Thread {
-            val port = 8080
-            val server = ServerSocket(port)
+            val socket = Socket("10.0.0.2", )
 
-            val socket = server.accept()
-
-//        // 스트림은 한 방향
-//        socket.getInputStream()     // 클라이언트로부터 들어오는 스트림 == 클라이언트의 socket.outputStream
-//        socket.getOutputStream()    // 클라이언트에게 데이터를 주는 스트림 == 클라이언트의 socket.inputStream
-
-            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-            val printer = PrintWriter(socket.getOutputStream())
-
-            var input: String? = "-1"
-
-            while(input != null && input != "") {
-                input = reader.readLine()
-            }
-            Log.e("SERVER", "READ DAT $input")
-
-            printer.println("HTTP/1.1 200 OK")
-            printer.println("Content-Type: text/html\r\n")
-
-            printer.println("<h1>Hello World</h1>")
-            printer.println("\r\n")
-            printer.flush()
-            printer.close()
-
-            reader.close()
-
-            socket.close()
         }.start()
+
 
     }
 }
