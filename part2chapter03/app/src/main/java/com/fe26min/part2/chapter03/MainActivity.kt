c72f00b968c368b89edeb3b16c4b18b997b9b174
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-10-12 15:35:44 +0900

    35.part02.ch02.04 Socket을 활용하여 Client 앱 만들기
    35.part02.ch02.04 Socket을 활용하여 Client 앱 만들기

--- part2chapter03/app/src/main/java/com/fe26min/part2/chapter03/MainActivity.kt
+++ part2chapter03/app/src/main/java/com/fe26min/part2/chapter03/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.ServerSocket
 import java.net.Socket
@@ -18,8 +19,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Thread {
-            val socket = Socket("10.0.0.2", )
+            try {
+                val socket = Socket("10.0.2.2", 8080)
+                val printer = PrintWriter(socket.getOutputStream())
+                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
 
+                printer.println("GET / HTTP/1.1")
+                printer.println("Host: 127.0.0.1:8080")
+                printer.println("User-Agent: android")
+                printer.println("\r\n")
+                printer.flush()
+
+                var input: String? = "-1"
+                while (input != null) {
+                    input = reader.readLine()
+                    Log.e("Client", "$input")
+                }
+
+                reader.close()
+                printer.close()
+                socket.close()
+            } catch (e : Exception) {
+                Log.e("Client", e.toString())
+            }
         }.start()
 
 
