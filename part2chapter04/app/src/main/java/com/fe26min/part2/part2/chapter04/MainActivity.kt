f433dc0b0520b50de86761c59b99537bdf187bb2
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-10-13 18:39:45 +0900

    36.part02.ch04.06. 유저 불러오기 검색 화면 구현하기(1)
    36.part02.ch04.06. 유저 불러오기 검색 화면 구현하기(1)

--- part2chapter04/app/src/main/java/com/fe26min/part2/part2/chapter04/MainActivity.kt
+++ part2chapter04/app/src/main/java/com/fe26min/part2/part2/chapter04/MainActivity.kt
@@ -3,6 +3,9 @@ package com.fe26min.part2.part2.chapter04
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fe26min.part2.part2.chapter04.adapter.UserAdapter
+import com.fe26min.part2.part2.chapter04.databinding.ActivityMainBinding
 import com.fe26min.part2.part2.chapter04.model.Repo
 import com.fe26min.part2.part2.chapter04.model.UserDto
 import com.fe26min.part2.part2.chapter04.network.GithubService
@@ -13,11 +16,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : MainActivity
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
@@ -25,9 +30,9 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val githubService = retrofit.create(GithubService::class.java)
-        githubService.listRepos("square").enqueue(object: Callback<List<Repo>> {
+        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
             override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                Log.e("MainActivity","List Repo : ${response.body().toString()}")
+                Log.e("MainActivity", "List Repo : ${response.body().toString()}")
             }
 
             override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
@@ -36,9 +41,21 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        githubService.searchUsers("square").enqueue(object: Callback<UserDto> {
+
+        val userAdapter = UserAdapter()
+
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
+        githubService.searchUsers("square").enqueue(object : Callback<UserDto> {
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
                 Log.e("MainActivity", "Search User : ${response.body().toString()}")
+
+                userAdapter.submitList(response.body()?.items)
+                
+
             }
 
             override fun onFailure(call: Call<UserDto>, t: Throwable) {
@@ -46,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
 
     }
 }
