3c9c1df5cdbaf549e5f1ceac9e8ff649a000fe04
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-10-24 14:08:40 +0900

    35.part02.ch08. 상세 페이지 UI 만들기
    35.part02.ch08. 상세 페이지 UI 만들기

--- lectures/part2chapter05/app/src/main/java/com/fe26min/part2/part2/chapter05/NewsAdapter.kt
+++ lectures/part2chapter05/app/src/main/java/com/fe26min/part2/part2/chapter05/NewsAdapter.kt
@@ -9,12 +9,16 @@ import com.bumptech.glide.Glide
 import com.fe26min.part2.part2.chapter05.databinding.ActivityMainBinding
 import com.fe26min.part2.part2.chapter05.databinding.ItemNewsBinding
 
-class NewsAdapter: ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil){
+class NewsAdapter(private val onClick: (String) -> Unit): ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil){
 
     inner class ViewHolder(private val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: NewsModel) {
             binding.titleTextView.text = item.title
+            binding.root.setOnClickListener{
+                onClick(item.link)
+            }
+
             Glide.with(binding.thumbnailImageView)
                 .load(item.imageUrl)
                 .into(binding.thumbnailImageView)
---
+++ lectures/part2chapter05/app/src/main/java/com/fe26min/part2/part2/chapter05/WebViewActivity.kt
@@ -0,0 +1,31 @@
+package com.fe26min.part2.part2.chapter05
+
+import android.os.Bundle
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.fe26min.part2.part2.chapter05.databinding.ActivityWebviewBinding
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityWebviewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val url = intent.getStringExtra("url")
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+
+        if (url.isNullOrEmpty()) {
+            Toast.makeText(this, "잘못된 URL 입니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            binding.webView.loadUrl(url)
+        }
+
+    }
+}
